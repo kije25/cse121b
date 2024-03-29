26d99d6baa15df974108b401e1e2e0f060e2692f
kije25 <kije25@gmail.com>
2024-03-28 22:01:36 -0600

    started w05-task

---
+++ w05-task/w05-task.js
@@ -0,0 +1,22 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.querySelector('#temples')
+
+let templeList = []
+
+/* async displayTemples Function */
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* filterTemples Function */
+
+
+getTemples();
+
+/* Event Listener */
